bc52ea2e25033e629ecc06d36d7a4da265684090
Aslan2142 <Aslan2142@gmail.com>
2021-04-06 11:11:02 +0200

    Added CORS support

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const config = require('./config.json');
 
 const express = require('express');
+const cors = require('cors');
 
 let server = express();
+server.use(cors());
 server.listen(config.port, config.host, () => {
     console.log('Server started on ' + config.host + ':' + config.port);
 });
